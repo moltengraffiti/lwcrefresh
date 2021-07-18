dab45b1ab2b91be9044d27f56112b9f324961def
Sam Reilly <samreilly@gmail.com>
2021-07-18 15:24:12 +1000

    Fixed iterator value

--- force-app/main/default/lwc/conditional/conditional.js
+++ force-app/main/default/lwc/conditional/conditional.js
@@ -2,6 +2,8 @@ import { LightningElement, track } from 'lwc';
 
 export default class Conditional extends LightningElement {
     @track displayDiv = false;
+    @track cityList = ["Sydney", "Dublin", "Cork", "Dubai"];
+
 
     showDivHandler(e) {
         this.displayDiv = e.target.checked;
