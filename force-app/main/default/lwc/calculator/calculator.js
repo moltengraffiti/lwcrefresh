75d93fee2628390ca6a67c1009e5ad6aef71bc2c
Sam Reilly <samreilly@gmail.com>
2021-07-18 16:40:30 +1000

    Calc exercise complete

--- force-app/main/default/lwc/calculator/calculator.js
+++ force-app/main/default/lwc/calculator/calculator.js
@@ -3,6 +3,8 @@ import { LightningElement, track } from 'lwc';
 export default class Calculator extends LightningElement {
 
     @track currentResult;
+    @track previousResults = [];
+    @track showPreviousResults = false;
 
     firstNumber;
     secondNumber;
@@ -26,22 +28,30 @@ export default class Calculator extends LightningElement {
         // let addResult = firstN + secondN;
         // this.currentResult = `Adding ${firstN} to ${secondN} equals ${addResult}`;
         this.currentResult = `Adding ${firstN} to ${secondN} equals ${firstN + secondN}`;
+        this.previousResults.push(this.currentResult);
     }
 
     subtractHandler(e) {
         const firstN = parseInt(this.firstNumber);
         const secondN = parseInt(this.secondNumber);
         this.currentResult = `Subtracting ${secondN} from ${firstN} equals ${firstN - secondN}`;
+        this.previousResults.push(this.currentResult);
     }
     multiplyHandler(e) {
         const firstN = parseInt(this.firstNumber);
         const secondN = parseInt(this.secondNumber);
         this.currentResult = `Multiplying ${firstN} by ${secondN} equals ${firstN * secondN}`;
+        this.previousResults.push(this.currentResult);
     }
     divideHandler(e) {
         const firstN = parseInt(this.firstNumber);
         const secondN = parseInt(this.secondNumber);
         this.currentResult = `Dividing ${firstN} by ${secondN} equals ${firstN / secondN}`;
+        this.previousResults.push(this.currentResult);
+    }
+
+    showPrevious(e) {
+        this.showPreviousResults = e.target.checked;
     }
 }
 
